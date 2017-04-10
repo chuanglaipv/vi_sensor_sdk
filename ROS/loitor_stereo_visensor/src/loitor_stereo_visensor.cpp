389fb3d5b58f983f856c04f42a132582d4a96b47
loitor-vis <1624376866@qq.com>
2017-04-11 00:04:40 +0800

    vi_sensor_sdk v1.3.1 Signed-off-by: loitor-vis <1624376866@qq.com>

--- ROS/loitor_stereo_visensor/src/loitor_stereo_visensor.cpp
+++ ROS/loitor_stereo_visensor/src/loitor_stereo_visensor.cpp
@@ -48,6 +48,7 @@ void* imu_data_stream(void *)
 	int counter=0;
 	imu_start_transfer=false;
 
+
 	while((!visensor_Close_IMU_viewer)&&!imu_start_transfer)usleep(1000);
 	while(!visensor_Close_IMU_viewer)
 	{
@@ -55,9 +56,13 @@ void* imu_data_stream(void *)
            	{
 			counter++;
 			// 每隔20帧显示一次imu数据
-			if(counter>=20)
+			if(counter>=20) 
 			{
 				//cout<<"visensor_imudata_pack->a : "<<visensor_imudata_pack.ax<<" , "<<visensor_imudata_pack.ay<<" , "<<visensor_imudata_pack.az<<endl;
+				float ax=visensor_imudata_pack.ax;
+				float ay=visensor_imudata_pack.ay;
+				float az=visensor_imudata_pack.az;
+				//cout<<"visensor_imudata_pack->a : "<<sqrt(ax*ax+ay*ay+az*az)<<endl;
 				//cout<<"imu_time : "<<visensor_imudata_pack.imu_time<<endl;
 				//cout<<"imu_time : "<<visensor_imudata_pack.system_time.tv_usec<<endl;
 				counter=0;
@@ -66,15 +71,16 @@ void* imu_data_stream(void *)
 			imu_msg.header.frame_id = "/imu";
 			ros::Time imu_time;
 			imu_time.sec=visensor_imudata_pack.system_time.tv_sec;
-			imu_time.nsec=visensor_imudata_pack.system_time.tv_usec;
+			imu_time.nsec=1000*visensor_imudata_pack.system_time.tv_usec;
 			imu_msg.header.stamp = imu_time;
+			imu_msg.header.seq=0;
 
 			imu_msg.linear_acceleration.x=visensor_imudata_pack.ax;
 			imu_msg.linear_acceleration.y=visensor_imudata_pack.ay;
 			imu_msg.linear_acceleration.z=visensor_imudata_pack.az;
-			imu_msg.angular_velocity.x=visensor_imudata_pack.rx;
-			imu_msg.angular_velocity.y=visensor_imudata_pack.ry;
-			imu_msg.angular_velocity.z=visensor_imudata_pack.rz;
+			imu_msg.angular_velocity.x=3.1415926f*visensor_imudata_pack.rx/180.0f;
+			imu_msg.angular_velocity.y=3.1415926f*visensor_imudata_pack.ry/180.0f;
+			imu_msg.angular_velocity.z=3.1415926f*visensor_imudata_pack.rz/180.0f;
 			imu_msg.orientation.w=visensor_imudata_pack.qw;
 			imu_msg.orientation.x=visensor_imudata_pack.qx;
 			imu_msg.orientation.y=visensor_imudata_pack.qy;
@@ -82,7 +88,7 @@ void* imu_data_stream(void *)
 
 			pub_imu.publish(imu_msg);
 		}
-		usleep(50);
+		usleep(10);
 	}
 	pthread_exit(NULL);
 }
@@ -157,49 +163,68 @@ int main(int argc, char **argv)
  
 	// publish 到这两个 topic
 	image_transport::ImageTransport it(n);
-	image_transport::Publisher pub = it.advertise("/camera/left/image_raw", 1);
+	image_transport::Publisher pub = it.advertise("/cam0/image_raw", 1);
 	sensor_msgs::ImagePtr msg;
 
 	image_transport::ImageTransport it1(n);
-	image_transport::Publisher pub1 = it1.advertise("/camera/right/image_raw", 1);
+	image_transport::Publisher pub1 = it1.advertise("/cam1/image_raw", 1);
 	sensor_msgs::ImagePtr msg1;
 
 	// 使用camera硬件帧率设置发布频率
 	ros::Rate loop_rate((int)hardware_fps);
 
-	while (ros::ok()) 
+	int static_ct=0;
+
+	timeval img_time_test,img_time_offset;
+	img_time_test.tv_usec=0;
+	img_time_test.tv_sec=0;
+	img_time_offset.tv_usec=50021;
+	img_time_offset.tv_sec=0;
+
+	while (ros::ok())
 	{
 		imu_start_transfer=true;
+
+		//cout<<"visensor_get_hardware_fps() ==== "<<visensor_get_hardware_fps()<<endl;
 
 		if(visensor_cam_selection==0)
 		{
 
 			visensor_imudata paired_imu=visensor_get_stereoImg((char *)img_left.data,(char *)img_right.data,left_stamp,right_stamp);
 
+
 			// 显示同步数据的时间戳（单位微秒）
-			cout<<"left_time : "<<left_stamp.tv_usec<<endl;
-			cout<<"right_time : "<<right_stamp.tv_usec<<endl;
-			cout<<"paired_imu time ===== "<<paired_imu.system_time.tv_usec<<endl<<endl;
+			//cout<<"left_time : "<<left_stamp.tv_usec<<endl;
+			//cout<<"right_time : "<<right_stamp.tv_usec<<endl;
+			//cout<<"paired_imu time ===== "<<paired_imu.system_time.tv_usec<<endl<<endl;
+			//cout<<"visensor_get_hardware_fps() ==== "<<1.0f/visensor_get_hardware_fps()<<endl;
 
 			cv_bridge::CvImage t_left=cv_bridge::CvImage(std_msgs::Header(), "mono8", img_left);
 			cv_bridge::CvImage t_right=cv_bridge::CvImage(std_msgs::Header(), "mono8", img_right);
 
-			// 加时间戳
+			// 加时间戳(right_time=left_time)
 			ros::Time msg_time;
 			msg_time.sec=left_stamp.tv_sec;
-			msg_time.nsec=left_stamp.tv_usec;
+			msg_time.nsec=1000*left_stamp.tv_usec;
 			t_left.header.stamp = msg_time;
+			
 			ros::Time msg1_time;
-			msg1_time.sec=right_stamp.tv_sec;
-			msg1_time.nsec=right_stamp.tv_usec;
+			msg1_time.sec=left_stamp.tv_sec;
+			msg1_time.nsec=1000*left_stamp.tv_usec;
 			t_right.header.stamp = msg1_time;
+			t_right.header.seq=0;
+			t_left.header.seq=0;
 
 			msg = t_left.toImageMsg();
 			msg1 = t_right.toImageMsg();
 
-			pub.publish(msg);
-			pub1.publish(msg1);
-
+			static_ct++;
+			{
+				pub.publish(msg);
+				pub1.publish(msg1);
+				static_ct=0;
+			}
+			
 			// 显示时间戳
 			//cout<<"left_time : "<<left_stamp.tv_usec<<endl;
 			//cout<<"right_time : "<<right_stamp.tv_usec<<endl<<endl;
@@ -218,8 +243,9 @@ int main(int argc, char **argv)
 			// 加时间戳
 			ros::Time msg1_time;
 			msg1_time.sec=right_stamp.tv_sec;
-			msg1_time.nsec=right_stamp.tv_usec;
+			msg1_time.nsec=1000*right_stamp.tv_usec;
 			t_right.header.stamp = msg1_time;
+			t_right.header.seq=0;
 			
 			msg1 = t_right.toImageMsg();
 			
@@ -238,11 +264,20 @@ int main(int argc, char **argv)
 			// 加时间戳
 			ros::Time msg_time;
 			msg_time.sec=left_stamp.tv_sec;
-			msg_time.nsec=left_stamp.tv_usec;
+			msg_time.nsec=1000*left_stamp.tv_usec;
 			t_left.header.stamp = msg_time;
-			
+			t_left.header.seq=0;
+
+
 			msg = t_left.toImageMsg();
-			pub.publish(msg);
+
+			
+			static_ct++;
+			if(static_ct>=5)
+			{
+				pub.publish(msg);
+				static_ct=0;
+			}
 		}
 
 		ros::spinOnce(); 
